a4a1b1a1146df91f6310694127f48189230fece0
kevinXDks <120220989+kevinXDks@users.noreply.github.com>
2024-04-01 20:04:22 -0400

    Add files via upload

---
+++ Comandos.php
@@ -0,0 +1,21 @@
+<?php
+   $respuestas = array(
+       "thekingknight1",
+       "shennylorena",
+       "nicolasfnpvz",
+       "izumi_19281",
+       "uva656",
+       "sovieticoremasterizado",
+       "kevinxdks",
+       "yaninatuki",
+       "nicovolvoa",
+       "Tú",
+       "Tu papá",
+       "Tu vieja"
+       "tu hermana",
+       "Aver, chupenmela y yo confirmo"
+       "a mi no me engañas pa, Shenny es la mas trola de todos aca"
+       );
+   $random = array_rand($respuestas);
+   echo $respuestas[$random];
+?>
